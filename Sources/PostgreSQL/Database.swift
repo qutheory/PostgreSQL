d95fef7a0e797dc1d6f3523bbe621fe3ae07834e
John Bona <jbona88@gmail.com>
2017-02-28 00:44:02 -0500

    Add support for LISTEN and NOTIFY

--- Sources/PostgreSQL/Database.swift
+++ Sources/PostgreSQL/Database.swift
@@ -1,3 +1,5 @@
+import Core
+
 #if os(Linux)
     import CPostgreSQLLinux
 #else
@@ -97,7 +99,47 @@ public class Database {
             return []
         }
     }
+	
+	public func listen(to channel: String, callback: @escaping (Notification) -> Void) throws {
+		try background {
+			do {
+				let connection = try self.makeConnection()
+				
+				try self.execute("LISTEN \(channel)", on: connection)
+				
+				while true {
+					if connection.connected != true {
+						throw DatabaseError.cannotEstablishConnection(connection.error)
+					}
+					
+					PQconsumeInput(connection.connection)
+					
+					while let pgNotify = PQnotifies(connection.connection) {
+						let notification = Notification(relname: pgNotify.pointee.relname, extra: pgNotify.pointee.extra, be_pid: pgNotify.pointee.be_pid)
+						
+						callback(notification)
+						
+						PQfreemem(pgNotify)
+					}
+				}
+			}
+			catch {
+				fatalError("\(error)")
+			}
+		}
+	}
 
+	public func notify(channel: String, payload: String?, on connection: Connection? = nil) throws {
+		let connection = try connection ?? makeConnection()
+		
+		if let payload = payload {
+			try execute("NOTIFY \(channel), '\(payload)'", on: connection)
+		}
+		else {
+			try execute("NOTIFY \(channel)", on: connection)
+		}
+	}
+	
     public func makeConnection() throws -> Connection {
         return try Connection(host: self.host, port: self.port, dbname: self.dbname, user: self.user, password: self.password)
     }
