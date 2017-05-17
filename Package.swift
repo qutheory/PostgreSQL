7e2cc4f8c3ca032d6373230025d812585b6e9fb0
pruthvikar <pruthvikar@gmail.com>
2017-05-17 23:16:23 +0200

    Update Package.swift
    Updated cpostgresql to major version 2

--- Package.swift
+++ Package.swift
@@ -1,12 +1,10 @@
 import PackageDescription
 
-let beta = Version(2,0,0, prereleaseIdentifiers: ["beta"])
-
 let package = Package(
     name: "PostgreSQL",
     dependencies: [
         // Module map for `libpq`
-        .Package(url: "https://github.com/vapor-community/cpostgresql.git", beta),
+        .Package(url: "https://github.com/vapor-community/cpostgresql.git", majorVersion: 2),
 
         // Data structure for converting between multiple representations
         .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
