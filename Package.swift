1ebd08a64eac6d88689a4c000a1e2d5257823df3
pruthvikar <pruthvikar@gmail.com>
2017-05-17 20:12:58 +0200

    Update Package.swift
    Pinning using major version

--- Package.swift
+++ Package.swift
@@ -9,9 +9,9 @@ let package = Package(
         .Package(url: "https://github.com/vapor-community/cpostgresql.git", beta),
 
         // Data structure for converting between multiple representations
-        .Package(url: "https://github.com/vapor/node.git", Version(2,0,0)),
+        .Package(url: "https://github.com/vapor/node.git", majorVersion: 2),
 
         // Core extensions, type-aliases, and functions that facilitate common tasks
-        .Package(url: "https://github.com/vapor/core.git", Version(2,0,0)),
+        .Package(url: "https://github.com/vapor/core.git", majorVersion: 2),
     ]
 )
